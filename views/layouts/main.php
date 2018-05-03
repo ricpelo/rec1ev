565b19e44a26b937b1ce5127672a0b339dea8cbd
Ricardo Pérez <ricpelo@gmail.com>
2018-05-03 16:52:20 +0200

    Cambia el copyright

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -76,7 +76,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Ricardo Pérez <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
